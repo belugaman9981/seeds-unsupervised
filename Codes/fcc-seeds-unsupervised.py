08fc2f5ef7f01e1f2dd21d7ba3407cdf084aaf12
belugaman9981 <littledage@outlook.com>
2026-01-18 19:45:07 -0800

    Update fcc-seeds-unsupervised.py

--- Codes/fcc-seeds-unsupervised.py
+++ Codes/fcc-seeds-unsupervised.py
@@ -3,3 +3,10 @@ import pandas            as pd
 import seaborn           as sns
 import matplotlib.pyplot as plt
 
+
+cols = ["area", "perimeter", "compactness", "length", 
+        "width", "asymmetry", "groove", "class"]
+
+df = pd.read_csv("seeds_dataset.txt", 
+                 names=cols, sep="\s+")
+

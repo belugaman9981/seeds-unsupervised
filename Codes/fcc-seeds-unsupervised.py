854748f9c885bfd3452ffafa38592b7254657c87
belugaman9981 <littledage@outlook.com>
2026-01-18 19:47:44 -0800

    Update fcc-seeds-unsupervised.py

--- Codes/fcc-seeds-unsupervised.py
+++ Codes/fcc-seeds-unsupervised.py
@@ -11,3 +11,11 @@ df = pd.read_csv("seeds_dataset.txt",
                  names= cols, sep= "\s+")
 
 df.head()
+
+
+for i in range(len(cols)-1):
+  for j in range(i+1, len(cols)-1):
+    x_label = cols[i]
+    y_label = cols[j]
+    sns.scatterplot(x=x_label, y=y_label, data=df, hue='class')
+    plt.show()
